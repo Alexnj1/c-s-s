c5dbcf42c3cbf09aaeba249ff9448f3375a1368c
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-03-29 16:26:36 -0400

    updared adminroutes

--- controllers/routes/api/admin-routes.js
+++ controllers/routes/api/admin-routes.js
@@ -16,3 +16,52 @@ router.get('/', (req,res) => {
 })
 
 
+router.get('/', (req,res) => {
+    
+
+
+})
+
+
+
+
+router.get('/:id', (req,res) => {
+
+    Admin.findOne({
+    attributes: {exclude: ['password']},
+    where: {id: req.params.id},
+    include: [
+        {
+            model: Post,
+            attributes: ['post_title','post_content', 'admin_id', 'created_at']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_content', 'created_at'],
+            include: [{
+                model: Post,
+                attributes: ['title']
+            }]
+        }]
+    })
+
+.then(data => {
+    if (!data) {
+        res.status(404).json({ message: 'No user found' });
+        return
+    }
+    res.json(data)
+})  
+ .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+})
+
+
+router.post('/', (req,res) => {
+    Admin.create()
+
+
+})
+
