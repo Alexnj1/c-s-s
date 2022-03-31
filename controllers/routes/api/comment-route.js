fcbe4557967a153a103486215f795e1894c7ab77
Alex Noble-James <alexnoble143.an@gmail.com>
2022-03-31 19:53:59 -0400

    added comment route

--- controllers/routes/api/comment-route.js
+++ controllers/routes/api/comment-route.js
@@ -1,79 +1,91 @@
-const res = require('express/lib/response');
+const res = require("express/lib/response");
 //const PostCategory = require('../../../models/Post-Category');
 
-const router = require('express').Router();
-const { Admin, Comment, PostCategory, Post, User } = require('../../../models/relationships');
+const router = require("express").Router();
+const {
+  Admin,
+  Comment,
+  PostCategory,
+  Post,
+  User,
+} = require("../../../models/relationships");
 // const withAuth = require('../../utilities/auth')
 
 //GET ALL COMMENTS
 
-router.get('/', (req, res) => {
-    Comment.findAll({
-        attributes: ['comment_content'],
-        include: [
-            {
-                model: Post,
-                attributes: ['post_title', 'post_content']
-            },
-            {
-                model: User,
-                attributes: ['household_username']
-            },
-            {
-                model: Admin,
-                attributes: ['name', 'position',],
-            }]
-    })
-        .then(data => res.json(data))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-
-})
+router.get("/", (req, res) => {
+  Comment.findAll({
+    attributes: ["comment_content"],
+    include: [
+      {
+        model: Post,
+        attributes: ["post_title", "post_content"],
+      },
+      {
+        model: User,
+        attributes: ["household_username"],
+      },
+      {
+        model: Admin,
+        attributes: ["name", "position"],
+      },
+    ],
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // create a comment
 
-router.post('/', (req, res) => {
-    Comment.create({
-        comment_content: req.body.comment_content,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-        //admin_id: req.params.user_id,
-    })
-        .then(data => res.json(data))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-})
+router.post("/", (req, res) => {
+  Comment.create({
+    comment_content: req.body.comment_content,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+    admin_id: req.body.admin_id,
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //update a comment
 
-router.put('/:id', (req, res) => {
-
-    Comment.update(
-        { comment_content: req.params.comment_content },
-        {
-            where: {
-                id: req.params.id
-            }
-        })
-    .then(data => res.json(data))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+router.put("/:id", (req, res) => {
+  Comment.update(
+    { comment_content: req.params.comment_content },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-
-})
-
+});
 
 //delete a comment
 
-// router.delete(
-//     Comment.destroy(
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => {
+      res.status(200).json({status: 'success'});
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-//     )
-// )
-
-module.exports = router
+module.exports = router;
