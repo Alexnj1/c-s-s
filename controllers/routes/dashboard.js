b92e5180a587aa61a51c96c6c52251da80ae99c0
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-03-31 17:30:09 -0400

    updated routes index and dashboard

---
+++ controllers/routes/dashboard.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const sequelize = require('../../models/db/connection')
+
+const { Admin, Comment, PostCategory, Post, User } = require('../../models/relationships');
+
+
+
+//get all posts
+router.get('/', (req,res) => {
+    Post.findAll({
+        where: {user_id: req.session.user_id},
+        attributes: ['post_title', 'post_content', 'createdAt', 'updatedAt'],
+        include: [
+            {
+                model: User,
+                attributes: ['household_username']
+            },
+            {
+                model: Admin,
+                attributes: ['name', 'position',],
+            },
+        ]
+
+    })
+    .then(data => {
+        const allPosts = data.map(allPosts => allPosts.get({plain:true})) 
+        res.render('dashboard',{allPosts, loggedIn: true})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+router.get('', (req,res) =>{
+
+
+})
+
+
+
+router.get('/login',(req,res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return
+    }
+    res.render('login');
+})
+
+
+module.exports = router
--- controllers/routes/index.js
+++ controllers/routes/index.js
@@ -1,3 +1,11 @@
 const router = require('express').Router
 
 const apiRoute = require('./api/')
+const dashboard = require('./dashboard')
+
+
+
+router.use('/',apiRoute)
+router.use('/dashboard',dashboard)
+
+module.exports = router
