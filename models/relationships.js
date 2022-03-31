6357c998ed91a3375b5d7bd4cca0f4dc8f26a60a
Alex Noble-James <alexnoble143.an@gmail.com>
2022-03-31 13:19:30 -0400

    added post to comment relationshi[

--- models/relationships.js
+++ models/relationships.js
@@ -3,50 +3,59 @@ const Post = require("./Post");
 const PostCategory = require("./Post-Category");
 const Comment = require("./Comments");
 const Admin = require("./Admin");
+// const { hasMany } = require("./Admin");
 
 //========= HAS MANY ==========
 User.hasMany(Post, {
   foreignKey: "user_id",
-  onDelete: 'CASCADE'
+  onDelete: "CASCADE",
 });
 
 User.hasMany(Comment, {
-  foreignKey: 'user_id'
-})
+  foreignKey: "user_id",
+});
 
 Admin.hasMany(Post, {
   foreignKey: "admin_id",
 });
 
 Admin.hasMany(Comment, {
-  foreignKey: 'admin_id'
-})
+  foreignKey: "admin_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
 
 PostCategory.hasMany(Post, {
-  foreignKey: 'post_category_id'
-})
+  foreignKey: "post_category_id",
+});
 
 //========= BELONGS TO =========
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id',
-})
+  foreignKey: "user_id",
+});
 
 Post.belongsTo(Admin, {
-  foreignKey: 'admin_id'
-})
+  foreignKey: "admin_id",
+});
 
 Post.belongsTo(PostCategory, {
-  foreignKey: 'post_category_id'
-})
+  foreignKey: "post_category_id",
+});
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id'
-})
+  foreignKey: "user_id",
+});
 
 Comment.belongsTo(Admin, {
-  foreignKey: 'admin_id'
-})
+  foreignKey: "admin_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
 
 module.exports = {
   User,
